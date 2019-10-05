125e33920450a5cfe4ee26002d453bcbfff99b66
Scott <dixonsco@amazon.com>
2019-10-04 23:53:26 -0700

    more sophisticated toxing

---
+++ .buildkite/release.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# This software is distributed under the terms of the MIT License.
+#
+
+# +----------------------------------------------------------+
+# | BASH : Modifying Shell Behaviour
+# |    (https://www.gnu.org/software/bash/manual)
+# +----------------------------------------------------------+
+# Treat unset variables and parameters other than the special
+# parameters ‘@’ or ‘*’ as an error when performing parameter
+# expansion. An error message will be written to the standard
+# error, and a non-interactive shell will exit.
+set -o nounset
+
+# Exit immediately if a pipeline returns a non-zero status.
+set -o errexit
+
+# If set, the return value of a pipeline is the value of the
+# last (rightmost) command to exit with a non-zero status, or
+# zero if all commands in the pipeline exit successfully.
+set -o pipefail
+
+# +----------------------------------------------------------+
+
+export NANAIMO_VERSION=`cat src/nanaimo/version.py | grep __version__ | awk '{print $3 $4 $5}'`
+tox
+tox -e sonar | grep -v "sonar.login"
+tox -e upload | grep -v "twine upload"
