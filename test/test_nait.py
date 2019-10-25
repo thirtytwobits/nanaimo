afc290b2080e69ee4747b2e8778ff3faff3ff1cc
Scott <dixonsco@amazon.com>
2019-10-24 23:11:57 -0700

    fixing the build

--- test/test_nait.py
+++ test/test_nait.py
@@ -3,31 +3,30 @@
 # This software is distributed under the terms of the MIT License.
 #
 
-import fixtures
 import nanaimo
 import nanaimo.version
 
 
-def test_arg_version() -> None:
+def test_arg_version(run_nait) -> None:  # type: ignore
     """
     Verify --version argument contract.
     """
     expected = nanaimo.version.__version__
-    assert fixtures.run_nait(['--version']).stdout.decode('utf-8').startswith(expected)
+    assert run_nait(['--version']).stdout.decode('utf-8').startswith(expected)
 
 
-def test_help() -> None:
+def test_help(run_nait) -> None:  # type: ignore
     """
     Verify --help doesn't explode.
     """
-    result = fixtures.run_nait(['gtest_over_jlink', '--help']).stdout.decode('utf-8')
+    result = run_nait(['gtest_over_jlink', '--help']).stdout.decode('utf-8')
     assert len(result) > 4
     print(result)
 
 
-def test_nanaimo_bar() -> None:
+def test_nanaimo_bar(run_nait) -> None:  # type: ignore
     """
     Eat your dessert!
     """
-    result = fixtures.run_nait(['-vv', 'nanaimo_bar']).stdout.decode('utf-8')
+    result = run_nait(['-vv', 'nanaimo_bar']).stdout.decode('utf-8')
     print(result)
