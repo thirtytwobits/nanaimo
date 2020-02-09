c68e9b10490814f7370f4de9a0fd28effd3f0854
Scott <dixonsco@amazon.com>
2020-02-08 19:36:56 -0800

    Issue #116 and revisiting #112
    issue 116 stemmed from a misunderstanding of subprocess protocols in
    asyncio. Even though I fixed the test I decided to ultimatlyu remove it
    since it was a test of my own understanding of asyncio and not a valid
    unit tests of Nanaimo.
    
    After understanding the subprocess protocols better I realized that #
    112 wasn't fixed properly. I have revisited this issue and provided a
    superior fix.

--- test/test_subprocess.py
+++ test/test_subprocess.py
@@ -3,64 +3,17 @@
 # This software is distributed under the terms of the MIT License.
 #
 
-import asyncio
 import pathlib
 import re
-import sys
-import typing
 
 import pytest
 
-import material
 import nanaimo
 import nanaimo.fixtures
 
 
-class DummyProtocol(asyncio.SubprocessProtocol):
-
-    def __init__(self, exit_future: asyncio.Future):
-        self.exit_future = exit_future
-        self._count = 0
-
-    def pipe_data_received(self, fd: int, data: typing.Union[bytes, str]) -> None:
-        print(typing.cast(bytes, data).decode('utf-8'))
-        self._count += 1
-
-    def process_exited(self) -> None:
-        self.exit_future.set_result(True)
-
-
 @pytest.mark.asyncio
-@pytest.mark.timeout(15)
-async def test_continuous_subprocess(paths_for_test: material.Paths, event_loop: asyncio.AbstractEventLoop) -> None:
-    exit_future = asyncio.Future(loop=event_loop)  # type: typing.Any
-
-    code = """
-import sys
-with open('{}', 'r') as log_file:
-    while True:
-        line = log_file.readline()
-        if line:
-            sys.stdout.write(line)
-""".format(str(paths_for_test.long_text))
-
-    transport, protocol = await event_loop.subprocess_exec(lambda: DummyProtocol(exit_future),
-                                                           sys.executable,
-                                                           '-c',
-                                                           code,
-                                                           stdin=asyncio.subprocess.PIPE)
-
-    try:
-        await asyncio.wait_for(exit_future, 1)
-    except asyncio.TimeoutError:
-        pass
-
-    transport.close()
-
-
-@pytest.mark.asyncio
-async def test_subprocess_match_filter(nanaimo_fixture_manager: nanaimo.fixtures.FixtureManager,
-                                       mock_JLinkExe: pathlib.Path) -> None:
+async def test_subprocess_match_filter(mock_JLinkExe: pathlib.Path) -> None:
     """
     Executing JLinkExe breaks subprocess output stream filtering.
     We replicate this by searching the output for a simple error
@@ -71,10 +24,11 @@ async def test_subprocess_match_filter(nanaimo_fixture_manager: nanaimo.fixtures
         argument_prefix = 'test'
 
         def on_construct_command(self, arguments: nanaimo.Namespace, inout_artifacts: nanaimo.Artifacts) -> str:
-            return '{} -CommanderScript MISSING_FILE_'.format(str(mock_JLinkExe))
+            return '{} --simulate-error'.format(str(mock_JLinkExe))
 
-    subprocess_fixture = TestSubprocessFixture(nanaimo_fixture_manager)
-    filter = nanaimo.fixtures.SubprocessFixture.SubprocessMessageMatcher(re.compile('Simulating normal exit.'))
+    subprocess_fixture = TestSubprocessFixture(nanaimo.fixtures.FixtureManager())
+    filter = nanaimo.fixtures.SubprocessFixture.SubprocessMessageMatcher(re.compile('Simulating abnormal exit.'))
     subprocess_fixture.stdout_filter = filter
+    subprocess_fixture.stderr_filter = filter
     await subprocess_fixture.gather(test_log_stdout=True)
     assert filter.match_count == 1
